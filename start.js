909b8a9a234e32b18770261cf022d6edaaf0ec5d
Arod1207 <arod1207@me.com>
2020-07-02 23:30:36 -0500

    add inquirer functionality

--- start.js
+++ start.js
@@ -1,17 +1,40 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'Poorboi22',
-    // database : 'my_db'
-  });
-   
-  connection.connect();
-   
-  connection.query(err => {
-    console.log('Connected to mysql');
-  });
-   
-  connection.end();
+  host: "localhost",
+  user: "root",
+  password: "Poorboi22",
+  database: "employee_db",
+});
+
+connection.connect();
+
+connection.query((err) => {
+  console.log("Connected to mysql");
+
+  startEmployee();
+});
+
+
+
+connection.end();
+
+function startEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "This is a test",
+          name: "test",
+        },
+      ])
+      .then((answers) => {
+        console.log(answers.test);
+      })
+      .catch((err) => {
+        if (err) throw err;
+      });
+  }
